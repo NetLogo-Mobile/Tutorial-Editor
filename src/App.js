1369e727117956c37bcc75da817507a2f589fca9
ivyxuan <iloveivyxuan@gmail.com>
2020-08-06 17:23:12 -0500

    Add eslint and prettier to check and format code; Fix code format

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Form from "@rjsf/semantic-ui";
+import Form from '@rjsf/semantic-ui';
 import './App.css';
 
 function App() {
@@ -10,69 +10,67 @@ function App() {
 
   useEffect(() => {
     fetch(`${process.env.PUBLIC_URL}/tutorial-schema.json`)
-    .then((res) => res.json())
-    .then((data) => {
-      setSchema(data);
-    });
+      .then((res) => res.json())
+      .then((data) => {
+        setSchema(data);
+      });
   }, []);
 
   useEffect(() => {
     fetch(`${process.env.PUBLIC_URL}/ui-schema.json`)
-    .then((res) => res.json())
-    .then((data) => {
-      setUISchema(data);
-    });
+      .then((res) => res.json())
+      .then((data) => {
+        setUISchema(data);
+      });
   }, []);
 
-  const onSubmit = ({formData}, error) => {
-    downloadFormData(formData);
-  }
-
-  const downloadFormData = (formData) => {
-    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(formData, 0, 2));
+  const downloadFormData = (form) => {
+    const dataStr = `data:text/json;charset=utf-8, ${encodeURIComponent(
+      JSON.stringify(form, 0, 2)
+    )}`;
     const download = document.createElement('a');
-    download.setAttribute("href", dataStr);
-    download.setAttribute("download", "form-data.json");
+    download.setAttribute('href', dataStr);
+    download.setAttribute('download', 'form-data.json');
     document.body.appendChild(download);
     download.click();
     download.remove();
-  }
+  };
+
+  const onSubmit = ({ form }) => {
+    downloadFormData(form);
+  };
 
   const onFileChange = ({ target: { files } }) => {
     setSelectFile(files[0]);
-  }
+  };
 
   const uploadFile = () => {
     if (!selectFile) {
-      return
+      return;
     }
 
     const reader = new FileReader();
 
     reader.onload = (event) => {
-      try {
-        const json = JSON.parse(event.target.result);
-        setFormData(json);
-      } catch (error) {
-        alert('error when trying to parse json = ' + error);
-      }
-    }
+      const json = JSON.parse(event.target.result);
+      setFormData(json);
+    };
 
     reader.readAsText(selectFile);
-  }
+  };
 
   return (
-    <div className = "App">
-      <input type="file"
-             accept = ".json"
-             onChange = { onFileChange } />
-      <button onClick = { uploadFile }>
+    <div className="App">
+      <input type="file" accept=".json" onChange={onFileChange} />
+      <button type="button" onClick={uploadFile}>
         Upload
       </button>
-      <Form schema = { schema }
-            uiSchema = { uiSchema }
-            formData = { formData }
-            onSubmit = { onSubmit } />
+      <Form
+        schema={schema}
+        uiSchema={uiSchema}
+        formData={formData}
+        onSubmit={onSubmit}
+      />
     </div>
   );
 }
