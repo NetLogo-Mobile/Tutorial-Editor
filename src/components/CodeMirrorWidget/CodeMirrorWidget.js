833135494a4520c65f1e9a85c0773f1249715934
ivyxuan <iloveivyxuan@gmail.com>
2020-08-13 16:01:10 -0500

    Fix codemirror style

--- src/components/CodeMirrorWidget/CodeMirrorWidget.js
+++ src/components/CodeMirrorWidget/CodeMirrorWidget.js
@@ -1,8 +1,11 @@
 import React, { Fragment } from 'react';
 import { Form } from 'semantic-ui-react';
+import 'codemirror/addon/selection/active-line';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/trailingspace';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
-import { UnControlled as CodeMirror } from 'react-codemirror2';
+import { Controlled as CodeMirror } from 'react-codemirror2';
 import './code-mirror-widget.css';
 require('../../lib/netlogo.js');
 require('../../lib/netlogo.css');
@@ -11,7 +14,6 @@ const CodeMirrorWidget = (props) => {
   const { placeholder, options, onChange, required, value, label } = props;
   const defaultComments = `;;${placeholder}\n` || `;;To write netlogo code`;
   const _onChange = (value) => {
-    console.log(value);
     onChange && onChange(value === '' ? options.emptyValue : value);
   };
   return (
@@ -24,8 +26,15 @@ const CodeMirrorWidget = (props) => {
             mode: 'netlogo',
             theme: 'netlogo-default',
             lineNumbers: true,
+            lineWrapping: true,
+            styleActiveLine: true,
+            styleActiveSelected: true,
+            matchBrackets: true,
+            viewportMargin: 10,
+            showTrailingSpace: true,
+            viewportMargin: Infinity,
           }}
-          onChange={(editor, data, value) => {
+          onBeforeChange={(editor, data, value) => {
             _onChange(value);
           }}
         />
